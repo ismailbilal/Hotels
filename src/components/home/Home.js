cff1476067fe7efd15a284234ad2bc55aac61b78
ismailbilal <is123bilal@gmail.com>
2022-04-23 13:29:31 +0000

    add header and its style

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,47 @@
-import { StyledHome } from "./StyledHome";
+import { useEffect, useState } from "react";
+import {
+  StyledBackgroung,
+  StyledContiner,
+  StyledHome,
+  StyledItem,
+} from "./StyledHome";
+import backgroundImage1 from "../../images/homeBg.jpg";
+import backgroundImage2 from "../../images/homeBg2.jpg";
+const transparant = "rgba(0, 0, 0, .5)";
 
 const Home = () => {
-  return <StyledHome></StyledHome>;
+  const [background, setBackground] = useState(backgroundImage1);
+
+  const changeBackground = () => {
+    if (window.pageYOffset >= window.innerHeight) {
+      setBackground(backgroundImage2);
+    } else {
+      setBackground(backgroundImage1);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeBackground);
+    return () => {
+      window.removeEventListener("scroll", changeBackground);
+    };
+  }, []);
+
+  return (
+    <StyledHome>
+      <StyledBackgroung bg={background} />
+      <StyledContiner>
+        <StyledItem bg={transparant}></StyledItem>
+        <StyledItem className="changeable" primary>
+          hello
+        </StyledItem>
+        <StyledItem bg={transparant}></StyledItem>
+        <StyledItem className="changeable" primary>
+          hello
+        </StyledItem>
+      </StyledContiner>
+    </StyledHome>
+  );
 };
+
+export default Home;
--- src/components/home/StyledHome.js
+++ src/components/home/StyledHome.js
@@ -1,5 +1,36 @@
 import styled from "styled-components";
 
-const StyledHome = styled.div``;
+const StyledHome = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%;
+`;
 
-export { StyledHome };
+const StyledBackgroung = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center;
+`;
+
+const StyledContiner = styled.div`
+  position: absolute;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledItem = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: ${(props) => props.bg || "var(--background)"};
+  color: var(--on-background);
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export { StyledHome, StyledBackgroung, StyledContiner, StyledItem };
