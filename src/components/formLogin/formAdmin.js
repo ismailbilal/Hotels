83cc52f819c9acac79e0cbeac58a62dd08eda1eb
ismailbilal <is123bilal@gmail.com>
2022-04-25 03:30:00 +0000

    add form authentification for the admin

---
+++ src/components/formLogin/formAdmin.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { StyledContainer, StyledContent, StyledForm } from "./StyledFormAuth";
+
+export default () => {
+  const navigate = useNavigate();
+
+  const goToLoginPage = () => navigate("/login");
+
+  const gotToHome = () => navigate("/home");
+
+  const login = (e) => {
+    e.preventDefault();
+  };
+
+  const goToSignUpPage = () => {
+    navigate("/signup");
+  };
+
+  return (
+    <StyledContainer>
+      <button className="exit" onClick={gotToHome}>
+        <i className="fas fa-times"></i>
+      </button>
+      <StyledContent>
+        <div className="logo">
+          <img src="https://svgshare.com/i/_go.svg" alt="" />
+        </div>
+        <div className="image"></div>
+        <div className="text">
+          Start for free & get <br />
+          attractive offers today !
+        </div>
+      </StyledContent>
+      <StyledForm id="form" onSubmit={login}>
+        <div className="social">
+          <div className="title">Get Started</div>
+          <div className="question">
+            Don't Have an Account?
+            <br />
+            <span onClick={goToSignUpPage}>Sing Up</span>
+          </div>
+          <div className="btn">
+            <div className="btn-1">
+              <img src="https://img.icons8.com/color/30/000000/google-logo.png" />
+              Sign In
+            </div>
+            <div className="btn-2">
+              <img src="https://img.icons8.com/ios-filled/30/ffffff/facebook-new.png" />
+              Sign In
+            </div>
+          </div>
+          <div className="or">Or</div>
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <i className="far fa-envelope"></i>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="abc@gmail.com"
+          />
+          <i className="fas fa-exclamation-circle failure-icon"></i>
+          <i className="far fa-check-circle success-icon"></i>
+          <div className="error"></div>
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <i className="fas fa-lock"></i>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password here"
+          />
+          <i className="fas fa-exclamation-circle failure-icon"></i>
+          <i className="far fa-check-circle success-icon"></i>
+          <div className="error"></div>
+        </div>
+        <button id="btn" type="submit">
+          Sign In
+        </button>
+        <div className="admin">
+          Sign In as
+          <span onClick={goToLoginPage}> User</span>
+        </div>
+      </StyledForm>
+    </StyledContainer>
+  );
+};
