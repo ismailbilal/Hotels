8bcb9ee3b77f98d643698bce73b25c8aff094f79
ismailbilal <is123bilal@gmail.com>
2022-05-21 10:33:15 +0100

    input values was was inverted

--- src/components/hotel/RatingInput.js
+++ src/components/hotel/RatingInput.js
@@ -4,15 +4,15 @@ export default () => {
   return (
     <StyledRating>
       <div className="wrapper">
-        <input name="rating" type="radio" id="st1" value="1" />
+        <input name="rating" type="radio" id="st1" value="5" />
         <label htmlFor="st1"></label>
-        <input name="rating" type="radio" id="st2" value="2" />
+        <input name="rating" type="radio" id="st2" value="4" />
         <label htmlFor="st2"></label>
         <input name="rating" type="radio" id="st3" value="3" />
         <label htmlFor="st3"></label>
-        <input name="rating" type="radio" id="st4" value="4" />
+        <input name="rating" type="radio" id="st4" value="2" />
         <label htmlFor="st4"></label>
-        <input name="rating" type="radio" id="st5" value="5" />
+        <input name="rating" type="radio" id="st5" value="1" />
         <label htmlFor="st5"></label>
       </div>
     </StyledRating>
